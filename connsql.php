b4f2b29fc60c585c872e8d429ff8a4aa85439f7e
a884172003 <81687335+a884172003@users.noreply.github.com>
2021-06-03 14:34:19 +0800

    Create connsql.php

---
+++ connsql.php
@@ -0,0 +1,9 @@
+﻿<?php
+	$db_host = "localhost";  //資料庫主機設定
+	$db_username = "root";  //管理者帳號
+	$db_password = "a28110007";  //管理者密碼
+	//連線伺服器
+	$db_link = @mysql_connect($db_host, $db_username, $db_password);
+	if (!$db_link) die("資料庫連結失敗！");
+	mysql_query("SET NAMES 'utf8'");  //設定字元集與連線校對
+?>
